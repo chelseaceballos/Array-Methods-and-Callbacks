3000b8cfc48b6c88294ffb85132ff837268c6a8a
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-23 15:28:35 -0800

    completed task 3

--- index.js
+++ index.js
@@ -39,11 +39,14 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, getFinalsCB) {
+    const newArray = getFinalsCB(array).map(function(item){
+        return item.Year;
+    });
+    return newArray;
 }
 
-
+console.log(getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
