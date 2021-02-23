b5558cb5f1e983cd12a256e4b4cee99aca151054
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-23 15:41:32 -0800

    completed task 4

--- index.js
+++ index.js
@@ -55,9 +55,19 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-    /* code here */
+function getWinners(data, getFinalsCB) {
+    const finalsData = getFinalsCB(data);
+    const winners = finalsData.map(function(item){
+    if(item['Home Team Goals'] > item['Away Team Goals']){
+        return item['Home Team Name'];
+    } else {
+        return item['Away Team Name'];
+    }
+    })
+    return winners;
+    // return getFinalsCB(data).map(item => item['Home Team Goals'] > ['Away Team Goals'] ? item['Home Team Name'] : item['Away Team Name']);
 }
+console.log(getWinners(fifaData, getFinals));
 
 
 
